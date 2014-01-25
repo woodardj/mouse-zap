59185acdbf82e74efade74953be92f651d4f83f9
Jonathan Woodard <woodardj@gmail.com>
2014-01-25 12:04:07 -0700

    prefab updates for wind and fan

---
+++ mouseZap/Assets/Wind/WindBehavior.js
@@ -0,0 +1,20 @@
+﻿var airparticle : GameObject; // A little self-referential nonsense for bootstrapping
+
+function Start(){
+	bootstrapAir();	
+}
+
+function OnTriggerEnter(other : Collider) {
+	Debug.Log(other.gameObject.CompareTag("Fan"));
+	if (other.CompareTag("Fan") == true){
+		var blower = other.gameObject.GetComponent("FanControl").makeWind();
+		rigidbody.AddForce(blower);
+	}
+}
+
+function bootstrapAir(){
+	for(var i = 0 ; i < 5 ; i++){
+		var pos = Vector3(i, 0, i);
+		Instantiate(airparticle, pos, Quaternion.identity);
+	}
+}
