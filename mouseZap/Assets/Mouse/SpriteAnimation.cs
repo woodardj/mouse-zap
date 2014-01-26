441f37c3a25986420cca09a1e531b0386a07bf0a
Eve Park <github@eve-park.com>
2014-01-25 18:13:54 -0700

    Fixed animated fan

--- mouseZap/Assets/Mouse/SpriteAnimation.cs
+++ mouseZap/Assets/Mouse/SpriteAnimation.cs
@@ -17,36 +17,38 @@ public class SpriteAnimation : MonoBehaviour {
 		//Update
 		void Update () { 
 		SetSpriteAnimation(colCount,rowCount,rowNumber,colNumber,totalCells,fps);  
+		//ghettoOffset();
 	}
 
 	void ghettoOffset () {
-		offset = new Vector2 (0, .5f);
+		offset = (renderer.material.mainTextureOffset.y > 0) ?  new Vector2 (0, 0) : new Vector2 (0, .5f);
+		Debug.Log (offset.y);
 	}
 		
-		//SetSpriteAnimation
-		void SetSpriteAnimation(int colCount ,int rowCount ,int rowNumber ,int colNumber,int totalCells,int fps ){
-			
-			// Calculate index
-			int index  = (int)(Time.time * fps);
-			// Repeat when exhausting all cells
-			index = index % totalCells;
-			
-			// Size of every cell
-			float sizeX = 1.0f / colCount;
-			float sizeY = 1.0f / rowCount;
-			Vector2 size =  new Vector2(sizeX,sizeY);
-			
-			// split into horizontal and vertical index
-			var uIndex = index % colCount;
-			var vIndex = index / colCount;
-			
-			// build offset
-			// v coordinate is the bottom of the image in opengl so we need to invert.
-			float offsetX = (uIndex+colNumber) * size.x;
-			float offsetY = (1.0f - size.y) - (vIndex + rowNumber) * size.y;
-			Vector2 offset = new Vector2(offsetX,offsetY);
-			
-			renderer.material.mainTextureOffset = offset;
-			renderer.material.SetTextureScale  ("_MainTex", size);
-		}
+	//SetSpriteAnimation
+	void SetSpriteAnimation(int colCount ,int rowCount ,int rowNumber ,int colNumber,int totalCells,int fps ){
+		
+		// Calculate index
+		int index  = (int)(Time.time * fps);
+		// Repeat when exhausting all cells
+		index = index % totalCells;
+		
+		// Size of every cell
+		float sizeX = 1.0f / colCount;
+		float sizeY = 1.0f / rowCount;
+		Vector2 size =  new Vector2(sizeX,sizeY);
+		
+		// split into horizontal and vertical index
+		var uIndex = index % colCount;
+		var vIndex = index / colCount;
+		
+		// build offset
+		// v coordinate is the bottom of the image in opengl so we need to invert.
+		float offsetX = (uIndex+colNumber) * size.x;
+		float offsetY = (1.0f - size.y) - (vIndex + rowNumber) * size.y;
+		Vector2 offset = new Vector2(offsetX,offsetY);
+		
+		renderer.material.mainTextureOffset = offset;
+		renderer.material.SetTextureScale  ("_MainTex", size);
+	}
 }
