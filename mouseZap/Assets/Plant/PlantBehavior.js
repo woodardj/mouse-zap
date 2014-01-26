1e1d4c1e1f84367c903273a1805f8d356e1a2519
Jonathan Woodard <woodardj@gmail.com>
2014-01-25 20:43:17 -0700

    blargh

---
+++ mouseZap/Assets/Plant/PlantBehavior.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var hitPoints = 100;
+
+function Start () {
+
+}
+
+function Update () {
+ 	Debug.Log("updating");
+}
+
+function onCollisionEnter(collision: Collision){
+	Debug.Log(collision.relativeVelocity.magnitude);
+}
